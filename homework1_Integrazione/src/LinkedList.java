b3ef06d2ff56931b343186b66023438ee5234a4f
capu25 <73490102+capu25@users.noreply.github.com>
2023-09-19 17:38:51 +0200

    Primo Commit

---
+++ homework1_Integrazione/src/LinkedList.java
@@ -0,0 +1,97 @@
+public class LinkedList<T> {
+    private Node<T> head;
+    private int size;
+
+    public LinkedList() {
+        head = null;
+        size = 0;
+    }
+
+    public int size() {
+        return size;
+    }
+
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    public void add(T data) {
+        Node<T> newNode = new Node<>(data);
+        if (head == null) {
+            head = newNode;
+        } else {
+            Node<T> current = head;
+            while (current.getNext() != null) {
+                current = current.getNext();
+            }
+            current.setNext(newNode);
+        }
+        size++;
+    }
+
+    public boolean remove(T data) {
+        if (isEmpty()) {
+            return false;
+        }
+
+        if (head.getData().equals(data)) {
+            head = head.getNext();
+            size--;
+            return true;
+        }
+
+        Node<T> current = head;
+        Node<T> previous = null;
+
+        while (current != null && !current.getData().equals(data)) {
+            previous = current;
+            current = current.getNext();
+        }
+
+        if (current != null) {
+            previous.setNext(current.getNext());
+            size--;
+            return true;
+        }
+
+        return false;
+    }
+
+    public T get(int index) {
+        if (index < 0 || index >= size) {
+            throw new IndexOutOfBoundsException("Index out of bounds");
+        }
+
+        Node<T> current = head;
+        int currentIndex = 0;
+
+        while (currentIndex < index) {
+            current = current.getNext();
+            currentIndex++;
+        }
+
+        return current.getData();
+    }
+
+    private static class Node<T> {
+        private T data;
+        private Node<T> next;
+
+        public Node(T data) {
+            this.data = data;
+            this.next = null;
+        }
+
+        public T getData() {
+            return data;
+        }
+
+        public Node<T> getNext() {
+            return next;
+        }
+
+        public void setNext(Node<T> next) {
+            this.next = next;
+        }
+    }
+}
---
+++ homework1_Integrazione/src/Main.java
@@ -0,0 +1,13 @@
+public class Main{
+    public static void main(String[] args) {
+
+       LinkedList<Object> test = new LinkedList<>();
+
+       test.add("1");
+       test.remove("1");
+
+
+       System.out.println(test.isEmpty());
+
+    }
+}
